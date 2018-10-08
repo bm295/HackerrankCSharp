c0821afe8dcc20803366dbc57a8490e74545aac8
bm295 <baominh.nguyen.295@gmail.com>
2018-10-08 15:20:28 +0700

    Create Interfaces.cs

---
+++ Interfaces.cs
@@ -0,0 +1,13 @@
+public class Calculator : AdvancedArithmetic
+{
+    public int divisorSum(int n)
+    {
+        var sum = 0;
+        for(var i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
